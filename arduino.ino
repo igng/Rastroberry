ddc6bb3cdf754b175008634a4957202acf497017
igng <monti.d.andrea@gmail.com>
2016-10-08 16:36:08 +0200

    Changed filter position

--- arduino.ino
+++ arduino.ino
@@ -1,11 +1,60 @@
-// MPU-6050 Short Example Sketch
-// By Arduino User JohnChi
-// August 17, 2014
+// MPU-6050 Filtering
+// By Andrea Monti (igng)
+// Base on code of Arduino User JohnChi
 // Public Domain
 #include <Wire.h>
 
 const int MPU_addr = 0x68;                      // I2C address of the MPU-6050
-int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+int16_t raw_x_acc[3]    = {0};
+int16_t raw_y_acc[3]    = {0};
+int16_t raw_z_acc[3]    = {0};
+int16_t raw_temp[3] = {0};
+int16_t raw_x_gyro[3]   = {0};
+int16_t raw_y_gyro[3]   = {0};
+int16_t raw_z_gyro[3]   = {0};
+int16_t filt_x_acc[3]   = {0};
+int16_t filt_y_acc[3]   = {0};
+int16_t filt_z_acc[3]   = {0};
+int16_t filt_temp[3]    = {0};
+int16_t filt_x_gyro[3]  = {0};
+int16_t filt_y_gyro[3]  = {0};
+int16_t filt_z_gyro[3]  = {0};
+
+void update()
+{
+    raw_x_acc[2] = raw_x_acc[1];
+    raw_y_acc[2] = raw_y_acc[1];
+    raw_z_acc[2] = raw_z_acc[1];
+    raw_temp[2] = raw_temp[1];
+    raw_x_gyro[2] = raw_x_gyro[1];
+    raw_y_gyro[2] = raw_y_gyro[1];
+    raw_z_gyro[2] = raw_z_gyro[1] ;
+    
+    raw_x_acc[1] = raw_x_acc[0];
+    raw_y_acc[1] = raw_y_acc[0];
+    raw_z_acc[1] = raw_z_acc[0];
+    raw_temp[1] = raw_temp[0];
+    raw_x_gyro[1] = raw_x_gyro[0];
+    raw_y_gyro[1] = raw_y_gyro[0];
+    raw_z_gyro[1] = raw_z_gyro[0];
+    
+    filt_x_acc[2] = filt_x_acc[1];
+    filt_y_acc[2] = filt_y_acc[1];
+    filt_z_acc[2] = filt_z_acc[1];
+    filt_temp[2] = filt_temp[1];
+    filt_x_gyro[2] = filt_x_gyro[1];
+    filt_y_gyro[2] = filt_y_gyro[1];
+    filt_z_gyro[2] = filt_z_gyro[1];
+    
+    filt_x_acc[1] = filt_x_acc[0];
+    filt_y_acc[1] = filt_y_acc[0];
+    filt_z_acc[1] = filt_z_acc[0];
+    filt_temp[1] = filt_temp[0];
+    filt_x_gyro[1] = filt_x_gyro[0];
+    filt_y_gyro[1] = filt_y_gyro[0];
+    filt_z_gyro[1] = filt_z_gyro[0];
+}
+
 void setup()
 {
     Wire.begin();
@@ -17,25 +66,34 @@ void setup()
 }
 void loop()
 {
+    update();                                   // update of the local variables for the filter
     Wire.beginTransmission(MPU_addr);
     Wire.write(0x3B);                           // starting with register 0x3B (ACCEL_XOUT_H)
     Wire.endTransmission(false);
     Wire.requestFrom(MPU_addr ,14, true);       // request a total of 14 registers
-    AcX = (Wire.read() << 8) | Wire.read();
-    AcY = (Wire.read() << 8) | Wire.read();
-    AcZ = (Wire.read() << 8) | Wire.read();
-    Tmp = (Wire.read() << 8) | Wire.read();
-    GyX = (Wire.read() << 8) | Wire.read();
-    GyY = (Wire.read() << 8) | Wire.read();
-    GyZ = (Wire.read() << 8) | Wire.read();
+    raw_x_acc[0]  = (Wire.read() << 8) | Wire.read();
+    raw_y_acc[0]  = (Wire.read() << 8) | Wire.read();
+    raw_z_acc[0]  = (Wire.read() << 8) | Wire.read();
+    raw_temp[0]   = (Wire.read() << 8) | Wire.read();
+    raw_x_gyro[0] = (Wire.read() << 8) | Wire.read();
+    raw_y_gyro[0] = (Wire.read() << 8) | Wire.read();
+    raw_z_gyro[0] = (Wire.read() << 8) | Wire.read();
+    
+    filt_x_acc[0] = 1.8*filt_x_acc[1] - 0.81*filt_x_acc[2] + 0.0022676*(raw_x_acc[0] + 2*raw_x_acc[1] + raw_x_acc[2]);
+    filt_y_acc[0] = 1.8*filt_y_acc[1] - 0.81*filt_y_acc[2] + 0.0022676*(raw_y_acc[0] + 2*raw_y_acc[1] + raw_y_acc[2]);
+    filt_z_acc[0] = 1.8*filt_z_acc[1] - 0.81*filt_z_acc[2] + 0.0022676*(raw_z_acc[0] + 2*raw_z_acc[1] + raw_z_acc[2]);
+    filt_temp[0] = 1.8*filt_temp[1] - 0.81*filt_temp[2] + 0.0022676*(raw_temp[0] + 2*raw_temp[1] + raw_temp[2]);
+    filt_x_gyro[0] = 1.8*filt_x_gyro[1] - 0.81*filt_x_gyro[2] + 0.0022676*(raw_x_gyro[0] + 2*raw_x_gyro[1] + raw_x_gyro[2]);
+    filt_y_gyro[0] = 1.8*filt_y_gyro[1] - 0.81*filt_y_gyro[2] + 0.0022676*(raw_y_gyro[0] + 2*raw_y_gyro[1] + raw_y_gyro[2]);
+    filt_z_gyro[0] = 1.8*filt_z_gyro[1] - 0.81*filt_z_gyro[2] + 0.0022676*(raw_z_gyro[0] + 2*raw_z_gyro[1] + raw_z_gyro[2]);
 
-    Serial.print(AcX); Serial.print(" ");
-    Serial.print(AcY); Serial.print(" ");
-    Serial.print(AcZ); Serial.print(" ");
-    Serial.print(Tmp/340 + 36.53); Serial.print(" ");
-    Serial.print(GyX); Serial.print(" ");
-    Serial.print(GyY); Serial.print(" ");
-    Serial.println(GyZ);
+    Serial.print(filt_x_acc[0]); Serial.print(" ");
+    Serial.print(filt_y_acc[0]); Serial.print(" ");
+    Serial.print(filt_z_acc[0]); Serial.print(" ");
+    Serial.print(filt_temp[0]/340 + 36.53); Serial.print(" ");
+    Serial.print(filt_x_gyro[0]); Serial.print(" ");
+    Serial.print(filt_y_gyro[0]); Serial.print(" ");
+    Serial.println(filt_z_gyro[0]);
 
     delay(1);
 }
--- filtering.m
+++ filtering.m
@@ -6,7 +6,8 @@ clc;
 %%
 clc;
 flag = [0 1 0];                     % [accelerometer gyroscope temperature]
-fd = serial('/dev/ttyUSB0');
+fd = serial('/dev/ttyUSB3');
+hudps = dsp.UDPSender('RemoteIPPort',8888);
 
 err_x_acc = 0;
 err_x_acc_old = 0;
@@ -78,14 +79,8 @@ for i = 3:max_size
     A = fscanf(fd);
     B = textscan(A, '%d %d %d %f %d %d %d');
     [raw_x_acc(i), raw_y_acc(i), raw_z_acc(i), raw_temp(i), raw_x_gyro(i), raw_y_gyro(i), raw_z_gyro(i)] = deal(B{:});
-    
-    filt_x_acc(i) = 1.8*filt_x_acc(i-1) - 0.81*filt_x_acc(i-2) + 0.0022676*(raw_x_acc(i) + 2*raw_x_acc(i-1) + raw_x_acc(i-2));
-    filt_y_acc(i) = 1.8*filt_y_acc(i-1) - 0.81*filt_y_acc(i-2) + 0.0022676*(raw_y_acc(i) + 2*raw_y_acc(i-1) + raw_y_acc(i-2));
-    filt_z_acc(i) = 1.8*filt_z_acc(i-1) - 0.81*filt_z_acc(i-2) + 0.0022676*(raw_z_acc(i) + 2*raw_z_acc(i-1) + raw_z_acc(i-2));
-    
-    filt_x_gyro(i) = 1.8*filt_x_gyro(i-1) - 0.81*filt_x_gyro(i-2) + 0.0022676*(raw_x_gyro(i) + 2*raw_x_gyro(i-1) + raw_x_gyro(i-2));
-    filt_y_gyro(i) = 1.8*filt_y_gyro(i-1) - 0.81*filt_y_gyro(i-2) + 0.0022676*(raw_y_gyro(i) + 2*raw_y_gyro(i-1) + raw_y_gyro(i-2));
-    filt_z_gyro(i) = 1.8*filt_z_gyro(i-1) - 0.81*filt_z_gyro(i-2) + 0.0022676*(raw_z_gyro(i) + 2*raw_z_gyro(i-1) + raw_z_gyro(i-2));
+    data = num2cell([raw_x_acc(i), raw_y_acc(i), raw_z_acc(i), raw_temp(i), raw_x_gyro(i), raw_y_gyro(i), raw_z_gyro(i)]);
+    hudps(data)
     
     % Acceleration
     if (flag(1))
@@ -97,7 +92,7 @@ for i = 3:max_size
     if (flag(2))
         set(rgx, 'XData', t, 'YData', raw_x_gyro);
         set(rgy, 'XData', t, 'YData', raw_y_gyro);
-        set(rgz, 'XData', t, 'YData', raw_z_gyroc);
+        set(rgz, 'XData', t, 'YData', raw_z_gyro);
     end
     % Temperature
     if (flag(3))
@@ -106,53 +101,3 @@ for i = 3:max_size
     
     drawnow;
 end
-if (flag(1))
-    figure(1);
-    legend('Acc_x', 'Acc_y', 'Acc_z');
-    hold off;
-end
-if (flag(2))
-    figure(2);
-    legend('Gyro_x', 'Gyro_y', 'Gyro_z');
-    hold off;
-end
-if (flag(3))
-    figure(3);
-    legend('Temp');
-    hold off;
-end
-if (flag(1))
-    figure(11)
-    plot(t, raw_x_acc/16384); grid on;
-    hold on
-    plot(t, filt_x_acc/16384);
-    legend('Raw', 'Filtered');
-    figure(12)
-    plot(t, raw_y_acc/16384); grid on;
-    hold on
-    plot(t, filt_y_acc/16384);
-    legend('Raw', 'Filtered');
-    figure(13)
-    plot(t, raw_z_acc/16384); grid on;
-    hold on
-    plot(t, filt_z_acc/16384);
-    legend('Raw', 'Filtered');
-end
-if (flag(2))
-    figure(21)
-    plot(t, raw_x_gyro/131); grid on;
-    hold on
-    plot(t, filt_x_gyro/131);
-    legend('Raw', 'Filtered');
-    figure(22)
-    plot(t, raw_y_gyro/131); grid on;
-    hold on
-    plot(t, filt_y_gyro/131);
-    legend('Raw', 'Filtered');
-    figure(23)
-    plot(t, raw_z_gyro/131); grid on;
-    hold on
-    plot(t, filt_z_gyro/131);
-    legend('Raw', 'Filtered');
-end
-fclose(fd);
